af3ada560a3d5328fa1c9e14bfbd6dd2e03dc10e
zingoworks <zingoworks@gmail.com>
2019-07-21 23:23:08 +0900

    sunday

---
+++ src/main/ch7/Test.java
@@ -0,0 +1,15 @@
+package ch7;
+
+public class Test {
+
+    private static class Point {
+
+    }
+
+    public double xProjection(Point p1, Point p2) {
+        assert p1 != null : "p1 should not be null";
+
+        return 1.5;
+    }
+
+}
